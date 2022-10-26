b99aafafb613c66e47ff344e9d238d960eca05ff
Nilabha13 <nilabhasaha13@gmail.com>
2022-10-26 16:24:09 +0530

    Created FastChatDB Schema

---
+++ fastchatdb_schema.py
@@ -0,0 +1,44 @@
+import psycopg2
+
+conn = psycopg2.connect(host="localhost", port="5432", dbname="fastchatdb", user="postgres", password="pass123")
+
+cur = conn.cursor()
+
+users_create_query = '''CREATE TABLE IF NOT EXISTS USERS (
+                        user_id INT PRIMARY KEY,
+                        username TEXT,
+                        password_hash TEXT,
+                        is_online BOOLEAN,
+                        pending_individiual_messages_queue TEXT,
+                        pending_group_messages_queue TEXT
+                        );'''
+
+groups_create_query = '''CREATE TABLE IF NOT EXISTS GROUPS (
+                        group_id INT PRIMARY KEY,
+                        group_name TEXT,
+                        group_admin TEXT,
+                        group_members TEXT
+                        );'''
+
+individual_messages_create_query = '''CREATE TABLE IF NOT EXISTS INDIVIDUAL_MESSAGES(
+                                    individual_message_id INT PRIMARY KEY,
+                                    from_user_id INT,
+                                    to_user_id INT,
+                                    message_content TEXT
+                                    );'''
+
+group_messages_create_query = '''CREATE TABLE IF NOT EXISTS GROUP_MESSAGES(
+                                group_message_id INT PRIMARY KEY,
+                                group_id INT,
+                                from_user_id INT
+                                );'''
+
+cur.execute(users_create_query)
+cur.execute(groups_create_query)
+cur.execute(individual_messages_create_query)
+cur.execute(group_messages_create_query)
+
+conn.commit()
+
+cur.close()
+conn.close()
