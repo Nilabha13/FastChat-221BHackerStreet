1d01a26752fb1d3b14d9026807f470f81941080a
Woolverine563 <ameyadeshmukh563@gmail.com>
2022-11-21 16:56:20 +0530

    protocol added for server, minor changes in schema

--- multiple_servers.py
+++ multiple_servers.py
@@ -2,8 +2,11 @@ import socket, select
 import psycopg2
 import sys
 from utilities import *
-from constants import *
 
+def send_pending_messages(sock):
+	# a user is authenticated, send him stored messages from the database
+	sock.send(to_send({'command':'pending messages', 'messages':[]}))
+	pass
 
 def authenticate(sock, password):
 	global validated
@@ -22,10 +25,11 @@ def authenticate(sock, password):
 		print("password entered by user: ", password)
 		if(pass_real == password):
 			validated[sock] = 3
-			sock.send("Your password is validated! Welcome back to the chat room!\n".encode())
+			sock.send(to_send({'command' : 'password accepted'}))
+			send_pending_messages(sock)
 			#send_to_all(sock, f"\33[32m\33[1m\r Existing user {username} joined the conversation \n\33[0m")
 		else:
-			sock.send("Wrong password! Re-enter\n".encode())
+			sock.send(to_send({'command' : 're-enter'}))
 			validated[sock] += 1
 		cur.close()
 		conn.close()
@@ -42,11 +46,12 @@ def authenticate(sock, password):
 		print("password entered by user: ", password)
 		if(pass_real == password):
 			validated[sock] = 3
-			sock.send("Your password is validated! Welcome back to the chat room!\n".encode())
+			sock.send(to_send({'command' : 'password accepted'}))
+			send_pending_messages(sock)
 			#send_to_all(sock, f"\33[32m\33[1m\r Existing user {username} joined the conversation \n\33[0m")
 			cur.execute(f"UPDATE fastchatdb SET current_server_number = {number} WHERE username = '{username}'")
 		else:
-			sock.send("Wrong password too many times!\n".encode())
+			sock.send(to_send({'command' : 'error', 'type' : 'wrong password error'}))
 			del socket_name[sock]
 			del name_socket[username]
 			connected_list.remove(sock)
@@ -76,7 +81,7 @@ def add_new_user(sock, password):
 	conn.commit()
 	cur.close()
 	conn.close()
-	sock.send("Your password is received! Welcome to chat room!\n".encode())
+	sock.send(to_send({'command' : 'register for keyServer'}))
 	#send_to_all(sock, f"\33[32m\33[1m\r New user {record[(i,p)]} joined the conversation \n\33[0m")
 	validated[sock] = 3	
 
@@ -130,95 +135,105 @@ while True:
 			#the new connection is a client
 			else:
 				#handle cases
-				data = new_conn_socket.recv(1024).decode()
-				arr = data.split("-")
-				token = arr[0].strip()
-				client_name = arr[1].strip()
-				print(token, client_name)
-				if token in tokens:
-					tokens.remove(token)
-					connected_list.append(new_conn_socket)
-					socket_name[new_conn_socket] = client_name
-					name_socket[client_name] = new_conn_socket
-					#check if the user name received is in the database
-					existing_user = False
-					conn = psycopg2.connect(host="localhost", port="5432", dbname="fastchatdb", user="postgres", password="Ameya563")
-					cur = conn.cursor()
-					cur.execute(f"SELECT * FROM USERS WHERE username = '{client_name}'")
-					for i in cur.fetchall():
-						if(i[1] == client_name):
-							existing_user = True
-					cur.close()
-					conn.close()
-					if(existing_user):
-						validated[new_conn_socket] = 0
-						print("Client (%s, %s) attempting login" % addr," [",socket_name[new_conn_socket],"]")
-						new_conn_socket.send("\33[32m\r\33[1m Welcome back! Password: \n\33[0m".encode())
-					else:
-						validated[new_conn_socket] = -1
-						print("New client (%s, %s) registering" % addr," [",socket_name[new_conn_socket],"]")
-						new_conn_socket.send("\33[32m\r\33[1m Welcome to chat room! Please create a password: \n\33[0m".encode())	
-
+				data = new_conn_socket.recv(1024)
+				dict = from_recv(data)
+				try:
+					if dict['command'] == 'first connection' and dict['authentication token'] in tokens:
+						tokens.remove(dict['authentication token'])
+						connected_list.append(new_conn_socket)
+						client_name = dict['username']
+						socket_name[new_conn_socket] = client_name
+						name_socket[client_name] = new_conn_socket
+						#check if the user name received is in the database
+						existing_user = False
+						conn = psycopg2.connect(host="localhost", port="5432", dbname="fastchatdb", user="postgres", password="Ameya563")
+						cur = conn.cursor()
+						cur.execute(f"SELECT * FROM USERS WHERE username = '{client_name}'")
+						for i in cur.fetchall():
+							if(i[1] == client_name):
+								existing_user = True
+						cur.close()
+						conn.close()
+						if(existing_user):
+							validated[new_conn_socket] = 0
+							print("Client (%s, %s) attempting login" % addr," [",socket_name[new_conn_socket],"]")
+							new_conn_socket.send(to_send({'command' : 'existing user'}))
+						else:
+							validated[new_conn_socket] = -1
+							print("New client (%s, %s) registering" % addr," [",socket_name[new_conn_socket],"]")
+							new_conn_socket.send(to_send({'command' : 'new user'}))
+				except:
+					new_conn_socket.send(to_send({'command' : 'error', 'type' : 'wrong authentication token'}))
 		elif sock == lb_socket:
 			#Load balancer sending a token to validate
-			token = sock.recv(4096).decode().strip()
-			tokens.append(token)
+			token = sock.recv(4096)
+			dict = from_recv(token)
+			if dict['command' == 'authentication token']:
+				tokens.append(dict['token'])
 
 		elif sock in other_servers_sockets:
 			#other servers send "user2 - user1 : message", where user2 in connected list
-			data = sock.recv(4096).decode()
-			print(data)
-			arr = data.split("-", 1)
-			user2 = arr[0].strip()
-			if user2 in name_socket.keys():
-				user_sock = name_socket[user2]
-				if validated[user_sock] == 3:
-					print("here")
-					user_sock.send((arr[1].strip()).encode())
-		
+			data = sock.recv(4096)
+			dict = from_recv(data)
+			try:
+				if dict['command'] == 'user-user message':
+					user2 = dict['receiver username']
+					if user2 in name_socket.keys() and validated[name_socket[user2]] == 3:
+						user_sock = name_socket[user2]
+						user_sock.send(to_send(dict))
+					else:
+						#store the messages in the database
+						pass
+			except:
+				pass
 		else:
 			#a connected user sending a message
 			#case 1: a user trying to get autheticated
 			try:
-				if validated[sock] == -1:
-					password = sock.recv(1024).decode().strip()
+				dict = from_recv(sock.recv(4096))
+				if dict['command'] == 'new user':
+					password =dict['password']
 					add_new_user(sock, password)
-				elif validated[sock] < 3:
-					password = sock.recv(1024).decode().strip()
+				elif dict['command'] == 'password authentication':
+					password = dict['password']
 					authenticate(sock, password)
-				else:
+				elif dict['command'] == 'user-user message':
 					#user2 - message
 					sender = socket_name[sock]
-					arr = sock.recv(4096).decode().split("-")
-					user2 = arr[0].strip()
-					message = arr[1].strip()
-					print(message)
-					conn = psycopg2.connect(host="localhost", port="5432", dbname="fastchatdb", user="postgres", password="Ameya563")
-					cur = conn.cursor()
-					cur.execute(f"SELECT current_server_number FROM USERS WHERE username = '{user2}'")
-					db_data = cur.fetchall()
-					valid = False
-					if len(db_data) == 0:
-						sock.send("Username doesn't exist\n".encode())
-					else:
-						valid = True
-						receiver_server = db_data[0][0]
-					cur.close()
-					conn.close()
-					if valid:
-						if receiver_server == number and user2 in name_socket.keys():
-							user2_sock = name_socket[user2]
-							user2_sock.send(f"{sender} : {message}".encode())
+					if sender == dict['sender username']:
+						user2 = dict['receiver username']
+						message = dict['encrypted message']
+						print(message)
+						conn = psycopg2.connect(host="localhost", port="5432", dbname="fastchatdb", user="postgres", password="Ameya563")
+						cur = conn.cursor()
+						cur.execute(f"SELECT current_server_number FROM USERS WHERE username = '{user2}'")
+						db_data = cur.fetchall()
+						valid = False
+						if len(db_data) == 0:
+							sock.send(to_send({'command' : 'error', 'type' : 'user not found'}))
 						else:
-							if receiver_server < number:
-								receiver_port = 5000 + receiver_server
+							valid = True
+							receiver_server = db_data[0][0]
+						cur.close()
+						conn.close()
+						if valid:
+							if receiver_server == number:
+								if user2 in name_socket.keys() and validated[name_socket[user2]] == 3:
+									user2_sock = name_socket[user2]
+									user2_sock.send(to_send(dict))
+								else:
+									#store in the database
+									pass
 							else:
-								receiver_port = 5000 + 100 * receiver_server + number
-							for s in other_servers_sockets:
-								ip, r_port = s.getpeername()
-								print(r_port)
-								if r_port == receiver_port:
-									s.send(f"{user2} - {sender} : {message}".encode())
+								if receiver_server < number:
+									receiver_port = 5000 + receiver_server
+								else:
+									receiver_port = 5000 + 100 * receiver_server + number
+								for s in other_servers_sockets:
+									ip, r_port = s.getpeername()
+									#print(r_port)
+									if r_port == receiver_port:
+										s.send(to_send(dict))
 			except:
 				sender = socket_name[sock]
 				sock.close()
