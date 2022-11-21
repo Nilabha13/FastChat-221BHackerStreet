5036a7d5d7024936b4d1d458b0ba6bc62a566ddc
Nilabha13 <nilabhasaha13@gmail.com>
2022-11-21 15:44:56 +0530

    Translocated files

---
+++ crypto.py
@@ -0,0 +1,82 @@
+import bcrypt
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP, AES
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad, unpad
+from hashlib import sha256
+import random
+
+
+# RSA Key Handling
+
+def gen_RSA_keys():
+    keys = RSA.generate(2048)
+    return keys.public_key(), keys
+
+def export_key(key, filename):
+    keyfile = open(filename, 'wb')
+    keyfile.write(key.export_key('PEM'))
+    keyfile.close()
+
+def import_key(filename):
+    keyfile = open(filename, 'r')
+    key = RSA.import_key(keyfile.read())
+    keyfile.close()
+    return key
+
+
+# AES Key Handling
+
+def gen_AES_key_iv():
+	key = get_random_bytes(AES.block_size)
+	iv = get_random_bytes(AES.block_size)
+	return key, iv
+
+
+# AES Encryption/Decryption
+
+def encryptAES(key, iv, plaintext):
+	cipher = AES.new(key, AES.MODE_CBC, iv)
+	return cipher.encrypt(pad(plaintext, AES.block_size))
+	
+def decryptAES(key, iv, ciphertext):
+	cipher = AES.new(key, AES.MODE_CBC, iv)
+	return unpad(cipher.decrypt(ciphertext), AES.block_size)
+
+
+# RSA Encryption/Decryption
+
+def encryptRSA(key, plaintext):
+    cipher = PKCS1_OAEP.new(key)
+    return cipher.encrypt(plaintext)
+
+def decryptRSA(key, ciphertext):
+    cipher = PKCS1_OAEP.new(key)
+    return cipher.decrypt(ciphertext)
+
+
+# Registration and Login
+
+def get_password_hash(password):
+    return sha256(password).hexdigest()
+
+def generate_salt():
+    return bcrypt.gensalt()
+
+def hash_with_salt(salt, data):
+    return bcrypt.hashpw(data, salt)
+
+def verify_hash(salt, data, hsh):
+    return hsh == bcrypt.hashpw(data, salt)
+
+def hash_password_with_salt(salt, password):
+    return hash_with_salt(salt, get_password_hash(password))
+
+
+# Signatures
+
+def sign(private_key, data):
+    return encrypt(private_key, sha256(data).digest())
+
+def verify_signature(public_key, data, signature):
+    return decrypt(public_key, signature) == sha256(data).digest()
---
+++ keyserver.py
@@ -0,0 +1,54 @@
+import socket
+import select
+import psycopg2
+from constants import *
+from utilities import *
+
+
+connected_list = []
+port = KEYSERVER_PORT
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server_socket.bind(("localhost", port))
+server_socket.listen(10)
+
+while True:
+    rList, wList, eList = select.select([server_socket], [], [])
+
+    for sock in rList:
+        if sock == server_socket:
+            print("DEBUG: Incoming connection to server socket!")
+            sockfd, addr = server_socket.accept()
+            data = from_recv(sockfd.recv(4096))
+            print(f"DEBUG: Recevied data {data}")
+            if "command" in data:
+                conn = psycopg2.connect(host="localhost", port=DATABASE_PORT, dbname="fastchatdb", user="postgres", password="AshwinPostgre")
+                cur = conn.cursor()
+                command = data["command"]
+                if command == "STORE":
+                    username = data["username"]
+                    key = data["key"]
+                    cur.execute(f"SELECT * FROM KEYSERVER WHERE username='{username}'")
+                    if(len(cur.fetchall()) > 0):
+                        sockfd.send(to_send({"command": "ERROR", "msg": "User already exists!\n"}))
+                    else:
+                        cur.execute(f"INSERT INTO KEYSERVER VALUES ('{username}', '{key}')")
+                        conn.commit()
+                        sockfd.send(to_send({"command": "INFO", "msg": "Successfully stored!\n"}))
+                elif command == "RETRIEVE":
+                    username = data["username"]
+                    cur.execute(f"SELECT * FROM KEYSERVER WHERE username='{username}'")
+                    records = cur.fetchall()
+                    if len(records) == 0:
+                        sockfd.send(to_send({"command": "ERROR", "msg": "User not found\n"}))
+                    else:
+                        sockfd.send(to_send({"command": "PUBKEY", "pubkey": records[0][1]}))
+                else:
+                    sockfd.send(to_send({"command": "ERROR", "msg": "Command not recognised!\n"}))
+                cur.close()
+                conn.close()
+            else:
+                sockfd.send(to_send({"command": "ERROR", "msg": "Protocol not followed!\n"}))
+            
+            sockfd.close()
