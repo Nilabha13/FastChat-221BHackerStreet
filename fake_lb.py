36aaa46a57f81f4ca7117b8b65034ff6d6e6240c
210050024 <210050024@iitb.ac.in>
2022-11-21 20:35:36 +0530

    load balancer

--- fake_lb.py
+++ fake_lb.py
@@ -12,17 +12,7 @@ def round_robin():
 	return round_robin_index
 
 def num_of_clients():
-	conn = psycopg2.connect(host="localhost", port="5432", dbname="fastchatdb", user="postgres", password="AshwinPostgre")
-	cur = conn.cursor()
-	cur.execute("""SELECT count(*), current_server_number FROM USERS GROUP BY current_server_number""")
-	m_clients = 0
-	for i in cur.fetchall():
-		if i[0]<=m_clients:
-			m_clients=i[0]
-			m_server_name = i[1]+5000
-	cur.close()
-	conn.close()
-	return((m_server_name-5000)/100)
+    return num_clients_list.index(max(num_clients_list))+1
 
 def hash_port(port):
 	return port%5
@@ -37,7 +27,7 @@ def message_freq():
 
 
 
-
+num_clients_list = [0,0,0,0,0]
 
 NUM_SERVERS = 5
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -47,43 +37,55 @@ s.listen(10)
 choice = "number of clients"
 
 port_servers = {}
+list_port_servers = []
 valid_server_ports = []
 
 for i in range(1, NUM_SERVERS + 1):
     valid_server_ports.append(5000 + i * 100)
 
 while True:
-    rlist, wlist, elist = select.select([s], [], [])
-    if s in rlist:
-        new_conn_socket, addr = s.accept()
-        if addr[1] in valid_server_ports:
-            port_servers[addr[1]] = new_conn_socket
-            #new_conn_socket.send("Hello from LB".encode())
-            # fp(addr)
+    rlist, wlist, elist = select.select([s]+list_port_servers, [], [])
+    for sock in rlist:
+        if sock == s:
+            new_conn_socket, addr = s.accept()
+            if addr[1] in valid_server_ports:
+                port_servers[addr[1]] = new_conn_socket
+                list_port_servers.append(new_conn_socket)
+                #new_conn_socket.send("Hello from LB".encode())
+                # fp(addr)
+            else:
+                #handle client
+                #load balancing comes here
+                token = random.randint(10000, 20000)
+
+                if(choice=="round robin"):
+                    server_number = round_robin()
+                    server = 5000 + server_number
+
+                
+                if(choice=="number of clients"):
+                    server_number = num_of_clients()
+                    server = 5000 + server_number
+
+                
+                if(choice=="hash port"):
+                    server_number = hash_port(addr[1])
+                    server = 5000 + server_number
+                
+                l = [5001, 5002, 5003, 5004, 5005]
+                new_conn_socket.send(to_send({'command':'authentication token', 'server port':server, 'token':token}))
+                # fp(server)
+                port_servers[5000+ (server-5000)*100].send(to_send({'command':'authentication token', 'token':token}))
+                new_conn_socket.close()
         else:
-            #handle client
-            #load balancing comes here
-            token = random.randint(10000, 20000)
+            pinging_socket_number = (sock.getpeername()[1]-5000)/100
+            pinging_socket_data = from_recv(sock.recv(1024))
+            if pinging_socket_number["type"] == 'increase':
+                list_port_servers[pinging_socket_number-1] +=1
+            else:
+                list_port_servers[pinging_socket_number-1] -=1
 
-            if(choice=="round robin"):
-                server_number = round_robin()
-                server = 5000 + server_number
 
-            
-            if(choice=="number of clients"):
-                server_number = num_of_clients()
-                server = 5000 + server_number
-
-            
-            if(choice=="hash port"):
-                server_number = hash_port(addr[1])
-                server = 5000 + server_number
-            
-            l = [5001, 5002, 5003, 5004, 5005]
-            new_conn_socket.send(to_send({'command':'authentication token', 'server port':server, 'token':token}))
-            # fp(server)
-            port_servers[5000+ (server-5000)*100].send(to_send({'command':'authentication token', 'token':token}))
-            new_conn_socket.close()
 
             
 
