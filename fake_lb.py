d97d172f79e81e2eb8468d185ab2f40a8493a710
Nilabha13 <nilabhasaha13@gmail.com>
2022-11-24 15:36:18 +0530

    Updated modularised fake_lb and keyserver to be the actual files

--- fake_lb.py
+++ fake_lb.py
@@ -2,109 +2,106 @@ import socket, select
 import random
 from utilities import *
 
-logfd = open("logs/servers_logs/load_balancer.log", 'w')
-def log(msg):
-    log_to_file(msg, logfd)
 
-log("Load Balancer online!")
-
-round_robin_index = 1
+def gen_auth_token():
+    token = random.randint(10000, 20000)
+    log("Generated authentication token!")
+    return token
 
 def round_robin():
 	global round_robin_index
 	round_robin_index = (round_robin_index%5)+1
 	return round_robin_index
 
+
 def num_of_clients():
     return num_clients_list.index(min(num_clients_list))+1
 
+
 def hash_port(port):
-	return port%5
-
-# def response_time():
-# 	global connected_list
-# 	for i in connected_list:
-# 		i.send("ping".encode())
-
-def message_freq():
-	return 1
+	return port % NUM_SERVERS
 
 
+def get_server_recommendation(choice):
+    if(choice=="round robin"):
+        server_number = round_robin()
+        server = 5000 + server_number
+    elif(choice=="number of clients"):
+        server_number = num_of_clients()
+        server = 5000 + server_number
+    elif(choice=="hash port"):
+        server_number = hash_port(addr[1])
+        server = 5000 + server_number
 
-num_clients_list = [0,0,0,0,0]
+    log(f"Recommending server {server}")
+    return server
 
-NUM_SERVERS = 5
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('localhost', 5000))
-s.listen(10)
 
-choice = "number of clients"
+def send_auth_tokens(token, server):
+    port_servers[5000+ (server-5000)*100].send(to_send({'command':'authentication token', 'token':token}))
+    log("Sent authentication token to server!")
+    
+    new_conn_socket.send(to_send({'command':'authentication token', 'server port':server, 'token':token}))
+    log("Sent server recommendation and authentication token to client!")
 
-log(f"Load balancing strategy: {choice}")
 
-port_servers = {}
-list_port_servers = []
-valid_server_ports = []
+def handle_pinging_socket(sock):
+    pinging_socket_number = (sock.getpeername()[1]-5000)//100
+    pinging_socket_data = from_recv(sock.recv(1024))
+    if pinging_socket_data["type"] == 'increase':
+        num_clients_list[pinging_socket_number-1] +=1
+        log(f"Number of clients connected to {pinging_socket_number} incremented!")
+    else:
+        num_clients_list[pinging_socket_number-1] -=1
+        log(f"Number of clients connected to {pinging_socket_number} decremented!")
 
-for i in range(1, NUM_SERVERS + 1):
-    valid_server_ports.append(5000 + i * 100)
 
-while True:
-    rlist, wlist, elist = select.select([s]+list_port_servers, [], [])
-    for sock in rlist:
-        if sock == s:
-            log("Incoming connection to server socket!")
-            new_conn_socket, addr = s.accept()
-            if addr[1] in valid_server_ports:
-                log(f"Connection from server ({new_conn_socket.getpeername()}) accepted!")
-                port_servers[addr[1]] = new_conn_socket
-                list_port_servers.append(new_conn_socket)
-                #new_conn_socket.send("Hello from LB".encode())
-                # fp(addr)
+if __name__ == "__main__":
+    logfd = open("logs/servers_logs/load_balancer.log", 'w')
+    def log(msg):
+        log_to_file(msg, logfd)
+
+    log("Load Balancer online!")
+
+    round_robin_index = 1
+
+    num_clients_list = [0,0,0,0,0]
+
+    NUM_SERVERS = 5
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('localhost', 5000))
+    s.listen(10)
+
+    choice = "number of clients"
+
+    log(f"Load balancing strategy: {choice}")
+
+    port_servers = {}
+    list_port_servers = []
+    valid_server_ports = []
+
+    for i in range(1, NUM_SERVERS + 1):
+        valid_server_ports.append(5000 + i * 100)
+
+    while True:
+        rlist, wlist, elist = select.select([s]+list_port_servers, [], [])
+        for sock in rlist:
+            if sock == s:
+                log("Incoming connection to server socket!")
+                new_conn_socket, addr = s.accept()
+                if addr[1] in valid_server_ports:
+                    log(f"Connection from server ({new_conn_socket.getpeername()}) accepted!")
+                    port_servers[addr[1]] = new_conn_socket
+                    list_port_servers.append(new_conn_socket)
+                else:
+                    log(f"Connection from client ({new_conn_socket.getpeername()}) accepted!")
+
+                    token = gen_auth_token()
+                    server = get_server_recommendation(choice)
+                    send_auth_tokens(token, server)
+
+                    new_conn_socket.close()
+                    log("Connection closed!")
             else:
-                log(f"Connection from client ({new_conn_socket.getpeername()}) accepted!")
-                #handle client
-                #load balancing comes here
-                token = random.randint(10000, 20000)
-                log("Generated authentication token!")
-
-                if(choice=="round robin"):
-                    server_number = round_robin()
-                    server = 5000 + server_number
-
-                
-                if(choice=="number of clients"):
-                    server_number = num_of_clients()
-                    server = 5000 + server_number
-
-                
-                if(choice=="hash port"):
-                    server_number = hash_port(addr[1])
-                    server = 5000 + server_number
-
-                log(f"Recommending server {server}")
-                
-                l = [5001, 5002, 5003, 5004, 5005]
-                port_servers[5000+ (server-5000)*100].send(to_send({'command':'authentication token', 'token':token}))
-                log("Sent authentication token to server!")
-                
-                new_conn_socket.send(to_send({'command':'authentication token', 'server port':server, 'token':token}))
-                log("Sent server recommendation and authentication token to client!")
-                # fp(server)
-
-                new_conn_socket.close()
-                log("Connection closed!")
-        else:
-            pinging_socket_number = (sock.getpeername()[1]-5000)//100
-            pinging_socket_data = from_recv(sock.recv(1024))
-            if pinging_socket_data["type"] == 'increase':
-                num_clients_list[pinging_socket_number-1] +=1
-                log(f"Number of clients connected to {pinging_socket_number} incremented!")
-            else:
-                num_clients_list[pinging_socket_number-1] -=1
-                log(f"Number of clients connected to {pinging_socket_number} decremented!")
-
-
-
-            
+                handle_pinging_socket(sock)
 
---
+++ keyserver_old.py
@@ -0,0 +1,93 @@
+import socket
+import select
+import psycopg2
+from constants import *
+from utilities import *
+import crypto
+from base64 import b64encode
+
+
+connected_list = []
+port = KEYSERVER_PORT
+
+logfd = open("logs/servers_logs/keyserver.log", 'w')
+def log(msg):
+    log_to_file(msg, logfd)
+
+log("Keyserver online!")
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server_socket.bind(("localhost", port))
+server_socket.listen(10)
+
+while True:
+    rList, wList, eList = select.select([server_socket], [], [])
+
+    for sock in rList:
+        if sock == server_socket:
+            print("DEBUG: Incoming connection to server socket!")
+            log("Incoming connection to server socket!")
+            sockfd, addr = server_socket.accept()
+            print("DEBUG: Accepted connection")
+            log("Connection accepted!")
+            data = from_recv(sockfd.recv(4096))
+            print(f"DEBUG: Recevied data {data}")
+            log(f"Received response!")
+            if "command" in data:
+                conn = psycopg2.connect(host="localhost", port=DATABASE_PORT, dbname=DATABASE_NAME, user=DATABASE_USER, password=DATABASE_PASSWORD)
+                cur = conn.cursor()
+                log("Connected to database!")
+                command = data["command"]
+
+                if command == "STORE":
+                    username = data["username"]
+                    key = data["key"]
+                    type = data["type"]
+                    log(f"Attempting to store public key of {username}")
+                    if(type=="group"):
+                        cur.execute(f"DELETE FROM KEYSERVER WHERE username='{username}'")
+                        cur.execute(f"INSERT INTO KEYSERVER VALUES ('{username}', '{key}', '{type}')")
+                        conn.commit()
+                        print("[DEBUG]: Successfully stored")
+                        log("Successfully stored!")
+                        sockfd.send(to_send({"command": "INFO", "msg": "Successfully stored!\n"}))            
+                    else:
+                        cur.execute(f"SELECT * FROM KEYSERVER WHERE username='{username}' AND type='{type}'")
+                        if(len(cur.fetchall()) > 0):
+                            print("[DEBUG]: Error: User already exists")
+                            log("User already exists!")
+                            sockfd.send(to_send({"command": "ERROR", "msg": "User already exists!\n"}))
+                        else:
+                            cur.execute(f"INSERT INTO KEYSERVER VALUES ('{username}', '{key}', '{type}')")
+                            conn.commit()
+                            print("[DEBUG]: Successfully stored")
+                            log("Successfully stored!")
+                            sockfd.send(to_send({"command": "INFO", "msg": "Successfully stored!\n"}))
+                    
+                elif command == "RETRIEVE":
+                    username = data["username"]
+                    type = data["type"]
+                    log(f"Attempting to retrieve public key of {username}")
+                    cur.execute(f"SELECT * FROM KEYSERVER WHERE username='{username}' AND type='{type}'")
+                    records = cur.fetchall()
+                    if len(records) == 0:
+                        log("Error: User not found!")
+                        print("[DEBUG] Error: User not found")
+                        sockfd.send(to_send({"command": "ERROR", "msg": "User not found\n"}))
+                    else:
+                        log(f"Sent public key to {sockfd.getpeername()}")
+                        print(f"[DEBUG] Sent public key to {sockfd.getpeername()}")
+                        ks_privkey = crypto.import_key("KEYSERVER_PRIVKEY.pem")
+                        sockfd.send(to_send({"command": "PUBKEY", "pubkey": records[0][1], "signature": b64encode(crypto.sign(ks_privkey, records[0][1].encode())).decode()}))
+                else:
+                    log("Error: Command not recognised!")
+                    sockfd.send(to_send({"command": "ERROR", "msg": "Command not recognised!\n"}))
+                cur.close()
+                conn.close()
+            else:
+                log("Error: Protocol not followed!")
+                sockfd.send(to_send({"command": "ERROR", "msg": "Protocol not followed!\n"}))
+            
+            sockfd.close()
+            log("Connection closed!")
