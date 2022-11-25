8479a6338f6df45b7011b776d58f17ab0e0be835
Nilabha13 <nilabhasaha13@gmail.com>
2022-11-25 16:33:11 +0530

    Added partial documentation to message_patterns.py

--- message_patterns.py
+++ message_patterns.py
@@ -1,6 +1,23 @@
 import random
 
 def exponential_time_delay(N, mu, K, images_possible=False, images_prob=0.1):
+    """This is one of the messaging patterns used during performance analysis.
+    This simulates the sending of messages from a random user to another random user with an exponential time delay between two messages with the exponential parameter mu.
+    K such messages are sent across the network.
+
+    :param N: The number of clients in the network
+    :type N: int
+    :param mu: The parameter of the exponential distribution
+    :type mu: int / float
+    :param K: The number of messages to be sent across the network
+    :type K: int
+    :param images_possible: Could images be sent, defaults to False
+    :type images_possible: bool, optional
+    :param images_prob: If images can be sent, the probability of sending an image, defaults to 0.1
+    :type images_prob: float, optional
+    :return: The pattern of messages to be sent
+    :rtype: list
+    """
     patterns = []
     for k in range(K):
         sender_idx, receiver_idx = random.sample(range(N), 2)
@@ -13,6 +30,26 @@ def exponential_time_delay(N, mu, K, images_possible=False, images_prob=0.1):
 
 
 def fake_exponential_time_delay(N, mu, K, min_time, images_possible=False, images_prob=0.1):
+    """This is one of the messaging patterns used during performance analysis.
+    This simulates the sending of messages from a random user to another random user with an exponential time delay between two messages with the exponential parameter mu.
+    K such messages are sent across the network.
+    It is also ensured that the time delay is above a certain minimum threshold.
+
+    :param N: The number of clients in the network
+    :type N: int
+    :param mu: The parameter of the exponential distribution
+    :type mu: int / float
+    :param K: The number of messages to be sent across the network
+    :type K: int
+    :param min_time: The threshold minimum time delay between two messages being sent
+    :type min_time: float
+    :param images_possible: Could images be sent, defaults to False
+    :type images_possible: bool, optional
+    :param images_prob: If images can be sent, the probability of sending an image, defaults to 0.1
+    :type images_prob: float, optional
+    :return: The pattern of messages to be sent
+    :rtype: list
+    """
     patterns = []
     for k in range(K):
         sender_idx, receiver_idx = random.sample(range(N), 2)
@@ -27,6 +64,27 @@ def fake_exponential_time_delay(N, mu, K, min_time, images_possible=False, image
 
 
 def i_only_talk_to_bestie(N, mu, sigma, K, delta_t, images_possible=False, images_prob=0.1):
+    """This is one of the messaging patterns used during performance analysis.
+    This simulates a random user sending messages to a number of other users distributed according to a truncated discrete Gaussian with mean mu and standard deviation sigma.
+    K such random users are chosen and this process is repeated K times, with a time delay of delta_t between each such process.
+
+    :param N: The number of clients in the network
+    :type N: int
+    :param mu: The mean of the discrete Gaussian
+    :type mu: int / float
+    :param sigma: The standard deviation of the discrete Gaussian
+    :type sigma: int / float
+    :param K: The number of times the process is to be repeated
+    :type K: int
+    :param delta_t: The time delay between each execution of the process
+    :type delta_t: int / float
+    :param images_possible: Could images be sent, defaults to False
+    :type images_possible: bool, optional
+    :param images_prob: If images can be sent, the probability of sending an image, defaults to 0.1
+    :type images_prob: float, optional
+    :return: The pattern of messages to be sent
+    :rtype: list
+    """
     patterns = []
     for k in range(K):
         num_besties = max(min(round(random.gauss(mu, sigma)), N-1), 1)
