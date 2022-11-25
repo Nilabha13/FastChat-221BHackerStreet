45a555d1617e53a53cdc712de367a5be5119d72f
Nilabha13 <nilabhasaha13@gmail.com>
2022-11-25 23:51:01 +0530

    Improved nomenclature in message_patterns.py

--- message_patterns.py
+++ message_patterns.py
@@ -63,7 +63,7 @@ def fake_exponential_time_delay(N, mu, K, min_time, images_possible=False, image
     return patterns
 
 
-def i_only_talk_to_bestie(N, mu, sigma, K, delta_t, images_possible=False, images_prob=0.1):
+def burst_messages(N, mu, sigma, K, delta_t, images_possible=False, images_prob=0.1):
     """This is one of the messaging patterns used during performance analysis.
     This simulates a random user sending messages to a number of other users distributed according to a truncated discrete Gaussian with mean mu and standard deviation sigma.
     K such random users are chosen and this process is repeated K times, with a time delay of delta_t between each such process.
@@ -87,10 +87,10 @@ def i_only_talk_to_bestie(N, mu, sigma, K, delta_t, images_possible=False, image
     """
     patterns = []
     for k in range(K):
-        num_besties = max(min(round(random.gauss(mu, sigma)), N-1), 1)
+        num_peers = max(min(round(random.gauss(mu, sigma)), N-1), 1)
         sender_idx = random.randrange(N)
         receivers_possible = list(range(N)); receivers_possible.remove(sender_idx)
-        receivers_idx = random.sample(receivers_possible, num_besties)
+        receivers_idx = random.sample(receivers_possible, num_peers)
         for receiver_idx in receivers_idx:
             is_image = False
             if images_possible and random.random() <= images_prob:
@@ -225,8 +225,6 @@ def groups_transversal(num_groups, num_members, K, delta_t, images_possible=Fals
                 is_image = True
             patterns.append((group_idx, member_idx, delta_t, is_image))
     return patterns
-
-
 
 
 def group_creation_sample(num_members, overlap, num_groups):
