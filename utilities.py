a5546b643bcda9c870ae153064155d0fe67cf8d2
Nilabha13 <nilabhasaha13@gmail.com>
2022-11-25 17:43:15 +0530

    Completed documentation of utilities.py; Cleaned up utilities.py

--- utilities.py
+++ utilities.py
@@ -4,6 +4,7 @@ import os
 from struct import pack, unpack
 from matplotlib import pyplot as plt
 
+
 def to_send(dict):
 	"""Converts the supplied dictionary into appropriate JSON format
 
@@ -13,6 +14,7 @@ def to_send(dict):
 	:rtype: bytes
 	"""
 	return json.dumps(dict).encode()
+
 
 def from_recv(data):
 	"""Converts the supplied JSON data to a dictionary
@@ -24,8 +26,6 @@ def from_recv(data):
 	"""
 	return json.loads(data.decode())
 
-def fp(data): # Fake function
-    print(f"[DEBUG] {data} TYPE:{type(data)}")
 
 def log_to_file(msg, fd):
 	"""Logs the supplied message to the file specified
@@ -38,6 +38,7 @@ def log_to_file(msg, fd):
 	fd.write(f"[{str(datetime.datetime.now())}] {msg}\n")
 	fd.flush()
 
+
 def create_dir_if_not_exists(dir):
 	"""Checks if the supplied directory already exists; creates the directory if it doesn't exist.
 
@@ -47,22 +48,43 @@ def create_dir_if_not_exists(dir):
 	if not os.path.isdir(dir):
 		os.makedirs(dir)
 
-def create_dirs_if_not_exist_recursive(dir_seq):
 
+def create_dirs_if_not_exist_recursive(dir_seq):
+	"""Checks if supplied nested directories already exists; creates the directories which do not exist.
+
+	:param dir_seq: The sequence of nested directories in the path
+	:type dir_seq: list
+	"""
 	dir_so_far = ''
 	for dir in dir_seq:
 		dir_so_far = os.path.join(dir_so_far, dir)
 		create_dir_if_not_exists(dir_so_far)
 
+
 def my_send(sock, to_send):
+	"""A send function with the capabilities of sending large data.
+
+	:param sock: The socket through which the data would be sent
+	:type sock: socket.socket
+	:param to_send: The data to be sent
+	:type to_send: bytes
+	"""
 	length = pack('>Q', len(to_send))
-	# print(f"Sending {len(to_send)} bytes")
 	sock.sendall(length)
 	sock.sendall(to_send)
 
+
 def my_recv(sock, buff_size):
+	"""A receive function with the capabilities of receiving large data.
+
+	:param sock: The socket through which the data is received
+	:type sock: socket.socket
+	:param buff_size: The chunk size of the buffer
+	:type buff_size: int
+	:return: The complete data received from the socket
+	:rtype: bytes
+	"""
 	byte_size = sock.recv(8)
-	# print(byte_size)
 	(length,) = unpack('>Q', byte_size)
 	data = b''
 	while len(data) < length:
@@ -71,6 +93,17 @@ def my_recv(sock, buff_size):
 	return data
 
 
-def plot_histogram(data, lower, upper, step):
-	plt.hist(data, bins=range(lower, upper, step), color='green')
+def plot_histogram(data, lower, upper, bin_width):
+	"""A function to plot histograms.
+
+	:param data: The data on whcich the histogram is plotted
+	:type data: list
+	:param lower: The lower limit of the histogram
+	:type lower: int / float
+	:param upper: The upper limit of the histogram
+	:type upper: int / float
+	:param bin_width: The bin-width of the histogram
+	:type bin_width: int / floar
+	"""
+	plt.hist(data, bins=range(lower, upper, bin_width), color='green')
 	plt.show()
